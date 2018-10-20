09326800a65e6340e02c205c2e8196f95d03b0d4
Jonathan Troyer <jonathantroyer@outlook.com>
2018-10-20 18:41:22 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <sys/time.h>
+
+// Returns a random value between -1 and 1
+double getRand() {
+    return (double) rand() * 2 / (double) (RAND_MAX) - 1;
+}
+
+long double Calculate_Pi_Sequential(long long number_of_tosses) {
+    return 0;
+}
+long double Calculate_Pi_Parallel(long long number_of_tosses) {
+    return 0;
+}
+
+int main() {
+    struct timeval start, end;
+
+    long long num_tosses = 10000000;
+
+    printf("Timing sequential...\n");
+    gettimeofday(&start, NULL);
+    long double sequential_pi = Calculate_Pi_Sequential(num_tosses);
+    gettimeofday(&end, NULL);
+    printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
+
+    printf("Timing parallel...\n");
+    gettimeofday(&start, NULL);
+    long double parallel_pi = Calculate_Pi_Parallel(num_tosses);
+    gettimeofday(&end, NULL);
+    printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
+
+    // This will print the result to 10 decimal places
+    printf("π = %.10Lf (sequential)", sequential_pi);
+    printf("π = %.10Lf (parallel)", parallel_pi);
+
+    return 0;
+}
+
